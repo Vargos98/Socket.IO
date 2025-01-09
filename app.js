da2592bfda8434a8782de4d316aaf0690af22b94
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2025-01-09 10:46:44 +0530

    Completed basic setup

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
+const express = require('express');
+const http = require('http')
+const app = express();
+
 const socketIO = require('socket.io');
+
+app.set("view engine","ejs")
+
+const server = http.createServer(app);
+const io = socketIO(server);
+
+io.on("connection", ()=>{
+  console.log("connected");
+} )
+
+app.get('/',(req,res)=>{
+  res.render('index');
+})
+
+server.listen(3000,()=>{
+  console.log("Server is running on port 3000")  //localhost:3000 
+})
