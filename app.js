447e67ed4e3227abaf4b0105e5407a8a2b9f0955
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2025-01-09 12:19:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -17,14 +17,14 @@ const io = socketIO(server);
 // })
 
 io.on("connection", (socket)=>{
-  socket.on("typing",()=>{
-    socket.broadcast.emit("typing")
-  })
+ console.log("connected")
+ socket.join("room1")
 
 })
 
 // io.emit() : method is used to send data to everyone in the socket/peoples.
 //socket.emit() : method is used to send data to a single socket/person
+// socket.broadcast() : method is used to send data to all sockets except the one sending
 
 app.get('/', (req, res) => {
   res.render('index');
